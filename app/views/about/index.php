d0f15d8f514933ff6223062555df7dca0fc2ef42
anggarizkirifaldy <angga150703@gmail.com>
2021-01-27 10:50:08 +0700

    angga

---
+++ app/views/about/index.php
@@ -0,0 +1,5 @@
+<div class="container">
+    <h1>About me</h1>
+    <img src="<?= BASEURL; ?>/img/profile.jpg" alt=" Angga Rizki" width="25%" class="rounded-circle shadow">
+    <p>halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya seorang <?= $data['pekerjaan']; ?></p>
+    </div>
